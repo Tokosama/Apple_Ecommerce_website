8eebc47da0b0a5c81fbd5944290af28bee999841
Tokosama <anouartoko@gmail.com>
2024-12-07 00:14:58 +0100

    gestion du sous module

---
+++ ecommerce-admin/pages/api/auth/[...nextauth].js
@@ -0,0 +1,19 @@
+import NextAuth from 'next-auth'
+import GoogleProvider from 'next-auth/providers/google'
+import { MongoDBAdapter } from "@auth/mongodb-adapter"
+import client from '@/lib/db'
+import clientPromise from '@/lib/db'
+
+export default NextAuth({
+  providers: [
+    // OAuth authentication providers...
+    
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET
+    }),
+   
+  ],
+  adapter: MongoDBAdapter(clientPromise),
+
+})
