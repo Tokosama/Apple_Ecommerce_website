4983a353f9d96e8f4eb38c1a4a9f601aee613707
Tokosama <anouartoko@gmail.com>
2024-12-09 03:09:38 +0100

    Ajout de la fonctionnalite dajout dimage dun produit

--- ecommerce-admin/components/ProductForm.js
+++ ecommerce-admin/components/ProductForm.js
@@ -1,36 +1,64 @@
+/* eslint-disable @next/next/no-img-element */
 import { useState } from "react";
 import { useRouter } from "next/router";
 import axios from "axios";
 import Layout from "@/components/Layout";
+import { set } from "mongoose";
+import Spinner from "./spinner";
 
 export default function ProductForm({
   _id,
   title: existingTitle,
   description: existingDescription,
   price: existingPrice,
+  images: existingImages,
 }) {
   const [title, setTitle] = useState(existingTitle || "");
   const [description, setDescription] = useState(existingDescription || "");
   const [price, setPrice] = useState(existingPrice || "");
+  const [images, setImages] = useState(existingImages || []);
+  const [isUploading, setIsUploading] = useState(false)
   const [goToProducts, setGoToProducts] = useState(false);
   const router = useRouter();
   async function saveProduct(ev) {
     ev.preventDefault();
     const data = {
-      title,description,price,
+      title,
+      description,
+      price,
+      images
     };
     if (_id) {
       //update
-      await axios.put('/api/products', {...data,_id});
+      await axios.put("/api/products", { ...data, _id });
     } else {
       //create
-      await axios.post('/api/products', data);
+      await axios.post("/api/products", data);
     }
     setGoToProducts(true);
   }
   if (goToProducts) {
-    router.push('/products');
+    router.push("/products");
   }
+
+  async function uploadImages(ev) {
+
+    const files = ev.target?.files;
+    if (files?.length > 0) {
+      setIsUploading(true);
+      const data = new FormData();
+      for (const file of files) {
+        data.append("file", file);
+      }
+
+      const res = await axios.post("/api/upload", data);
+      setImages((oldImages) => {
+        return [...oldImages, ...res.data.links];
+      });
+      setIsUploading(false);
+    }
+  }
+
   return (
     <form onSubmit={saveProduct}>
       <h1 className="">New Product</h1>
@@ -41,6 +69,50 @@ export default function ProductForm({
         value={title}
         onChange={(ev) => setTitle(ev.target.value)}
       />
+
+      <label>Photos</label>
+      <div className="mb-2 flex flex-wrap gap-1">
+        {!!images?.length &&
+          images.map((link) => (
+            <div key={link} className=" h-24 ">
+              <img
+              className="rounded-lg"
+                src={link}
+                alt=""
+              />
+            </div>
+          ))}
+          {isUploading && (
+            <div className="h-24 p-1  flex items-center ">
+              <Spinner/>
+            </div>
+          )}
+        <label
+          className="w-24 h-24  text-center cursor-pointer
+        flex items-center justify-center text-sm gap-1 text-gray-500 rounded-lg bg-gray-200"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="size-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5m-13.5-9L12 3m0 0 4.5 4.5M12 3v13.5"
+            />
+          </svg>
+          <div>Upload</div>
+          <input
+            type="file"
+            onChange={uploadImages}
+            className="hidden"
+          ></input>
+        </label>
+      </div>
       <label>Description</label>
 
       <textarea
