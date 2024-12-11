3ccb2873094d5105a8289d4e51cc45a2ff8fb862
Tokosama <anouartoko@gmail.com>
2024-12-11 13:39:46 +0100

    Ajout de la securite d'authentification, verification des sessions , de l'admin , et refus d'acceder au page sans les droits

--- ecommerce-admin/pages/index.jsx
+++ ecommerce-admin/pages/index.jsx
@@ -6,7 +6,6 @@ import { useSession } from "next-auth/react";
 
 export default function Home() {
   const { data: session } = useSession();
-  if (!session) return;
   return (
     <>
       <Layout>
