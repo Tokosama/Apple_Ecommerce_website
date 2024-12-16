6591e9c9bfee07e552e608f624eb5bab52019818
Tokosama <anouartoko@gmail.com>
2024-12-16 12:30:48 +0100

    Gestion de l'affichage de la page success, du la reinitialisation des cart apres payement , et de l'ffichage des ordres

--- ecommerce-front/components/CartContext.js
+++ ecommerce-front/components/CartContext.js
@@ -1,38 +1,40 @@
-import { createContext, useEffect, useState } from "react";
+import {createContext, useEffect, useState} from "react";
 
 export const CartContext = createContext({});
 
-export function CartContextProvider({ children }) {
-  // pour pouvoir permettre au context de survivre lorsque la page est recharge
+export function CartContextProvider({children}) {
   const ls = typeof window !== "undefined" ? window.localStorage : null;
-  const [cartProducts, setCartProducts] = useState([]);
+  const [cartProducts,setCartProducts] = useState([]);
   useEffect(() => {
     if (cartProducts?.length > 0) {
-      ls?.setItem("cart", JSON.stringify(cartProducts));
+      ls?.setItem('cart', JSON.stringify(cartProducts));
     }
   }, [cartProducts]);
 
+
   useEffect(() => {
-    if (ls && ls.getItem("cart")) {
-      setCartProducts(JSON.parse(ls.getItem("cart")));
+    if (ls && ls.getItem('cart')) {
+      setCartProducts(JSON.parse(ls.getItem('cart')));
     }
   }, []);
-
   function addProduct(productId) {
-    setCartProducts((prev) => [...prev, productId]);
+    setCartProducts(prev => [...prev,productId]);
   }
-  function removeProduct(productId){
-    setCartProducts(prev =>{
+  function removeProduct(productId) {
+    setCartProducts(prev => {
       const pos = prev.indexOf(productId);
-      if(pos !== - 1){
-        return  prev.filter((value,index) => index !== pos)
+      if (pos !== -1) {
+        return prev.filter((value,index) => index !== pos);
       }
       return prev;
-    })
-
+    });
+  }
+  function clearCart() {
+    setCartProducts([]);
+    ls?.setItem('cart', JSON.stringify([]));
   }
   return (
-    <CartContext.Provider value={{ cartProducts, setCartProducts, addProduct, removeProduct }}>
+    <CartContext.Provider value={{cartProducts,setCartProducts,addProduct,removeProduct,clearCart}}>
       {children}
     </CartContext.Provider>
   );
