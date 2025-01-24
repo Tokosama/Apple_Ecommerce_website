bc5fde865d0bad39d8075b5624e91137ff06ffcc
Tokosama <anouartoko@gmail.com>
2025-01-24 20:54:52 +0100

    Ajout des pages categories et account (Upcoming....)

---
+++ ecommerce-front/pages/account.js
@@ -0,0 +1,30 @@
+import Center from "@/components/Center";
+import Header from "@/components/Header";
+import Title from "@/components/Title";
+import { mongooseConnect } from "@/lib/mongoose";
+import { Product } from "@/models/Product";
+import styled from "styled-components";
+
+
+
+export default function Account({products}) {
+  return (
+    <>
+      <Header />
+      <Center>
+        <Title>Upcoming.......</Title>
+      </Center>
+    </>
+  );
+}
+
+
+//Cest pour pouvoir recuper les elements de la bases de donnees
+export async function getServerSideProps(){
+    await mongooseConnect();
+    const products = await Product.find({},null, {sort:{'_id':-1}});
+
+    return {props:{
+            products: JSON.parse(JSON.stringify(products)),
+    }}
+}
